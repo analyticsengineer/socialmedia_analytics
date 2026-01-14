fb18873a95714c7da8c73e56fb12f91d7052ad81
Anuoluwapo Balogun <83256563+analyticsengineer@users.noreply.github.com>
2026-01-14 15:43:09 +0100

    Create Home_page.py

---
+++ ~/app/Home_page.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+
+# Set page config
+st.set_page_config(page_title="My Social Media Analytics", page_icon="👩🏼‍💻", layout="centered")
+
+# Title and intro
+st.title("👩🏼‍💻 My Social Media Analytics")
+st.markdown("#### Built for simplicity. Backed by clarity.")
+
+st.markdown(
+    """
+Welcome to my simple Social Media Analytics automation.  
+Tracking My Followers Growth, Post Likes, Reaches, Reshares, impressions and lots more... — all in one place.
+
+**What You’ll Find Here:**
+- 📆 **Account Growth**: Log and review my followers growth across all social media platforms like: X, Threads, Linkedin, Facebook, Instagram, TikTok, and Snapchat.
+- 📊 **Post Analytics**: Getting insights on my post to assist me with posting what truly matters to my online presence
+
+---
+
+- Growing my social media page organically
+
+
+"""
+)
